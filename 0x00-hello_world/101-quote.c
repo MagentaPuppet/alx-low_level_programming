1656f5d6d337da1064a9a6006c45b3c3ef7ab057
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-05-07 01:51:57 +0100

    added advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a string to stderr followed by a new line
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is
+			 useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
