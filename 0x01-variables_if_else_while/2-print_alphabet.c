a9456fa8c48c3044ffa09a64fb7de2a5dacdb20d
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-05-07 22:36:24 +0100

    added task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: prints the alphabet in lowercase
+ * main - prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
-{	
+{
 	int i;
 
 	for (i = 97; i <= 122; ++i)
