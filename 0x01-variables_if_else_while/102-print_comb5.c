ab3ac368fa62a96ab942c8751700998dba6c3bea
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-05-08 01:48:46 +0100

    added tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,13 +21,13 @@ int main(void)
 		{
 			for (k = '0'; k <= '9'; ++k)
 			{
-				for (l = '1'; l <= '9'; ++l)
+				for (l = '0'; l <= '9'; ++l)
 				{
-					num1 = ((i - 48)*10)+(j-48); /* creates a decimal number in the format ij */
-					num2 = ((k - 48)*10)+(l-48); /* creates a decimal number in the format kl */
-					
+					num1 = ((i - 48) * 10) + (j - 48); /* creates a number in the format ij */
+					num2 = ((k - 48) * 10) + (l - 48); /* creates a number in the format kl */
+
 					if (num1 < num2)
-					{	
+					{
 						putchar(i);
 						putchar(j);
 						putchar(' ');
