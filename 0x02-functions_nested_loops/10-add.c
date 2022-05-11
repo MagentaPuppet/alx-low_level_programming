40374f2c34612e6511d9e7f02623c7db53e0980a
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-05-12 00:23:19 +0100

    added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
