bd4606366d2d85524965055aaf3f4c6a764e360f
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-05-12 02:04:41 +0100

    added advanced tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,59 +8,39 @@
 
 void print_times_table(int n)
 {
-	int i, j, k, unit, tens, hundreds;
+	int i, j, k;
 
-	if (n < 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; ++i)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; ++j)
-			{	k = i * j;
-				unit = (k % 10) + 48;
-				tens = ((k / 10) % 10) + 48;
-				hundreds = (k / 100) + 48;
-				if (k < 10 && j != 0 && j != n)
-				{	_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(unit);
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
 					_putchar(',');
-				}
-				else if (k < 10 && j == n)
-				{	_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(unit);
-				}
-				else if (k >= 10 && k < 100 && j != n)
-				{	_putchar(' ');
 					_putchar(' ');
-					_putchar(tens);
-					_putchar(unit);
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
 					_putchar(',');
-				}
-				else if (k >= 10 && k < 100 && j == n)
-				{	_putchar(' ');
 					_putchar(' ');
-					_putchar(tens);
-					_putchar(unit);
-				}
-				else if (k >= 100 && j != n)
-				{	_putchar(' ');
-					_putchar(hundreds);
-					_putchar(tens);
-					_putchar(unit);
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
 					_putchar(',');
-				}
-				else if (k >= 100 && j == n)
-				{	_putchar(' ');
-					_putchar(hundreds);
-					_putchar(tens);
-					_putchar(unit);
-				}
-				else if (j == 0)
-				{	_putchar(unit);
-					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
