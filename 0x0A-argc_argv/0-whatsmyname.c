54749404a13e010d2a38648f32344ee74596099f
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-05-15 17:55:20 +0100

    added README.md and task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints its own name
+ * @argc: Number of elements in argv
+ * @argv: Array of strings passed as arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
