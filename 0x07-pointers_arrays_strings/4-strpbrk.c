3d0576c083d1a9c78d5fe4846001d4e5357764c8
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-06-26 21:56:20 +0100

    added task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -21,12 +21,13 @@ unsigned int len(char *s)
 }
 
 /**
- * _strspn - gets the length of a prefix substring
+ * _strpbrk - locates the first occurrence in the string @s
+ * of any of the bytes in the string @accept
  *
  * @s: pointer to area of memory to be searched
  * @accept: substring to be located
- * Return: Number of bytes in the initial segment of "s"
- * which consists only of bytes from "accept"
+ * Return: Pointer to the byte in @s that matches one of
+ * the bytes in @accept, or NULL if no such byte is found
  */
 
 char *_strpbrk(char *s, char *accept)
@@ -34,9 +35,9 @@ char *_strpbrk(char *s, char *accept)
 	unsigned int i;
 	unsigned int j;
 
-	for (i = 0; i <= len(s); ++i)
+	for (i = 0; i < len(s); ++i)
 	{
-		for (j = 0; j <= len(accept); ++j)
+		for (j = 0; j < len(accept); ++j)
 		{
 			if (*(s + i) == *(accept + j))
 			{
