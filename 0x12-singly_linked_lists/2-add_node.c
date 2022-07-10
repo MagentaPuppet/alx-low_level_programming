e826192453b3516501ebd723d16a2cbdfde93ffd
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-07-10 23:58:40 +0100

    added task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * len - returns the length of a string
+ * @str: string whose length is to be determined
+ *
+ * Return: the length of the string @str
+ **/
+
+unsigned int len(char *str)
+{
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the list_t list
  * @str: string to be added to the list_t list
@@ -17,7 +35,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (head && temp)
 	{
 		temp->str = string;
-		temp->len = strlen(string);
+		temp->len = len(string);
 
 		temp->next = *head;
 		*head = temp;
