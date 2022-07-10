24409deeb696256f570dd79b3490438aac04bb16
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-07-10 15:18:45 +0100

    added task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,29 @@
-#include "main.h"
+#include "lists.h"
 
 /**
- * @brief
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to be printed
  *
+ * Return: the number of nodes printed
  */
 
 size_t print_list(const list_t *h)
 {
-	while (/* condition */)
+	size_t num = 0;
+
+	while (h != NULL)
 	{
-		/* code */
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		num++;
 	}
 
-	return ;
+	return (num);
 }
