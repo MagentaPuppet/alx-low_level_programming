6c7073aca11010acaa394998ececb28a097ccaf7
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-07-11 12:34:57 +0100

    added task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * len - returns the length of a string
- * @str: string whose length is to be determined
- *
- * Return: the length of the string @str
- **/
-
-unsigned int len(char *str)
-{
-	unsigned int len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the list_t list
  * @str: string to be added to the list_t list
@@ -29,6 +11,12 @@ unsigned int len(char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
 	temp = malloc(sizeof(list_t));
 	if (!temp)
@@ -37,7 +25,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	temp->str = strdup(str);
-	temp->len = len(temp->str);
+	temp->len = len;
 
 	temp->next = *head;
 	*head = temp;
